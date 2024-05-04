d21fbbdc0e2df6d29c97640cb8e71b4daf99ec48
Alaina Baiju <141582704+Alainabaiju01@users.noreply.github.com>
2024-05-04 18:36:04 +0530

    Update firebase.js

--- hosp/src/firebase/firebase.js
+++ hosp/src/firebase/firebase.js
@@ -1,20 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";  
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCmc5SKWkLMeYUdb-weNHU-r_A1p_eBHI8",
   authDomain: "connectred.firebaseapp.com",
   projectId: "connectred",
   storageBucket: "connectred.appspot.com",
   messagingSenderId: "188929971012",
-  appId: "1:188929971012:web:7e3b5c29f9cfd68ef23d11",
-  measurementId: "G-XBQV3YV9XE"
-
+  appId: "1:188929971012:web:0f6d7528d27afc4ef23d11",
+  measurementId: "G-QS3R1LP05H"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
-const  auth = getAuth(app);
-
-export {app,analytics,auth};
