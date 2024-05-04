4101571c58eca39e33b570df3f5a203b937bf5ff
Alaina Baiju <141582704+Alainabaiju01@users.noreply.github.com>
2024-05-04 18:36:44 +0530

    Update auth.js

--- hosp/src/firebase/auth.js
+++ hosp/src/firebase/auth.js
@@ -1,22 +1,18 @@
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider,doSignInWithGoogle, signInWithPopup } from "firebase/auth";
 
-
-// Function to handle sign-in with Google
-export const signInWithGoogle = async () => {
-  try {
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    console.log("User signed in with Google:", user);
-    return user;
-  } catch (error) {
-    console.error("Error signing in with Google:", error.message);
-    throw error;
-  }
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
 };
 
-// Function to handle user sign-up with email and password
-export const signInWithEmailAndPassword = async (email, password) => {
-  // Your signUpWithEmailAndPassword function implementation
+export const doSignInWithEmailAndPassword = async (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => { 
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup (auth,provider);
+  return result;
+};
+export const doSignOut = () => {
+  return auth.signOut();
 };
